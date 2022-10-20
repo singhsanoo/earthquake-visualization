6a2ea6a338e6265c4cb53bd551fa8ab61b66b147
sanoo <sanookumar17@gmail.com>
2022-10-20 00:45:15 -0500

    added tectonic plates layer

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,47 +1,56 @@
-let myMap = L.map("map", {
-  center: [37.7749, -110.4194],
-  zoom: 5,
-});
+let url =
+  "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
 
-L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+// Define letiables for our tile layers.
+let street = L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
   attribution:
     '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-}).addTo(myMap);
+});
 
 let topo = L.tileLayer("https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png", {
   attribution:
     'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)',
 });
 
-let url =
-  "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
-
-d3.json(url).then(function (response) {
-  for (let i = 0; i < response.features.length; i++) {
-    let location = response.features[i];
-    let magnitude = response.features[i].properties.mag;
-    let place = response.features[i].properties.place;
-    let depth = location.geometry.coordinates[2];
-
-    console.log(magnitude);
-
-    if (location) {
-      L.circle(
-        [location.geometry.coordinates[1], location.geometry.coordinates[0]],
-        {
-          radius: magnitude * 15000,
-          fillColor: getColor(depth),
-          color: "black",
-          fillOpacity: 0.8,
-        }
-      )
-        .addTo(myMap)
-        .bindPopup(
-          `<b>Location:</b> ${place} <hr> <b>Magnitude:</b> ${magnitude} <br> <b>Depth:</b> ${depth}`
-        );
-    }
+let googleSat = L.tileLayer(
+  "http://{s}.google.com/vt/lyrs=s&x={x}&y={y}&z={z}",
+  {
+    maxZoom: 20,
+    subdomains: ["mt0", "mt1", "mt2", "mt3"],
   }
+);
+
+// Only one base layer can be shown at a time.
+let baseMaps = {
+  Street: street,
+  Topography: topo,
+  Satellite: googleSat,
+};
+
+// Tectonic Plates
+let t_url =
+  "https://raw.githubusercontent.com/fraxen/tectonicplates/master/GeoJSON/PB2002_boundaries.json";
+
+let myStyle = {
+  color: "#FC4E2A",
+  weight: 5,
+  stroke: true,
+  opacity: 1,
+};
+
+let tectonicLayer = L.geoJSON();
+
+d3.json(t_url).then(function (t_response) {
+  tectonicLayer.addData(t_response);
+  tectonicLayer.setStyle(myStyle);
 });
+
+// Optional if reading geoJson data from save file
+// let tectonicLayer;
+// $.getJSON("static/data/tectonic_boundaries.json", function (data) {
+//   console.log(data);
+//   tectonicLayer = L.geoJSON(data, { style: myStyle });
+// });
 
 function getColor(d) {
   return d > 90
@@ -57,23 +66,74 @@ function getColor(d) {
     : "#8EFF2B";
 }
 
-let legend = L.control({ position: "bottomright" });
+let placeMarkers = [];
+d3.json(url).then(function (response) {
+  for (let i = 0; i < response.features.length; i++) {
+    let location = response.features[i];
+    let magnitude = response.features[i].properties.mag;
+    let place = response.features[i].properties.place;
+    let depth = location.geometry.coordinates[2];
 
-legend.onAdd = function () {
-  let div = L.DomUtil.create("div", "info legend");
-  let depth = [-10, 10, 30, 50, 70, 90];
+    // console.log(magnitude);
 
-  // loop through our density intervals and generate a label with a colored square for each interval
-  for (let i = 0; i < depth.length; i++) {
-    div.innerHTML +=
-      '<i style="background:' +
-      getColor(depth[i] + 1) +
-      '"></i> ' +
-      depth[i] +
-      (depth[i + 1] ? "&ndash;" + depth[i + 1] + "<br>" : "+");
+    if (location) {
+      placeMarkers.push(
+        L.circle(
+          [location.geometry.coordinates[1], location.geometry.coordinates[0]],
+          {
+            radius: magnitude * 15000,
+            fillColor: getColor(depth),
+            color: "black",
+            fillOpacity: 0.8,
+          }
+        )
+          // .addTo(myMap)
+          .bindPopup(
+            `<b>Location:</b> ${place} <hr> <b>Magnitude:</b> ${magnitude} <br> <b>Depth:</b> ${depth}`
+          )
+      );
+    }
   }
 
-  return div;
-};
+  let earthquakeLayer = L.layerGroup(placeMarkers);
 
-legend.addTo(myMap);
+  console.log(placeMarkers);
+
+  // Overlays that can be toggled on or off
+  let overlayMaps = {
+    Earthquakes: earthquakeLayer,
+    "Tectonic Plates": tectonicLayer,
+  };
+
+  // Create a map object, and set the default layers.
+  let myMap = L.map("map", {
+    center: [30, -80],
+    zoom: 4,
+    layers: [street, earthquakeLayer],
+  });
+
+  // Pass our map layers into our layer control.
+  // Add the layer control to the map.
+  L.control.layers(baseMaps, overlayMaps, { collapsed: false }).addTo(myMap);
+
+  let legend = L.control({ position: "bottomright" });
+
+  legend.onAdd = function () {
+    let div = L.DomUtil.create("div", "info legend");
+    let depth = [-10, 10, 30, 50, 70, 90];
+
+    // loop through our density intervals and generate a label with a colored square for each interval
+    for (let i = 0; i < depth.length; i++) {
+      div.innerHTML +=
+        '<i style="background:' +
+        getColor(depth[i] + 1) +
+        '"></i> ' +
+        depth[i] +
+        (depth[i + 1] ? "&ndash;" + depth[i + 1] + "<br>" : "+");
+    }
+
+    return div;
+  };
+
+  legend.addTo(myMap);
+});
